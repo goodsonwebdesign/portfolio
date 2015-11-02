b0d778daea52bc2c160b5b1c29be87813dd0de58
Chase <chase@productivityapex.com>
2015-11-02 11:06:37 -0500

    Initial Commit

---
+++ app.js
@@ -0,0 +1,63 @@
+﻿angular.module("app", [])
+    .controller("mainctrl",
+    function ($scope, $timeout) {
+        //Initialize the switches to false.
+        $scope.switches = {
+            lamps: false
+        }
+
+        $scope.flipSwitch = function (switchName) {
+            switch(switchName) {
+                case 'lamps':
+                    if ($scope.switches.lamps == true) {
+                        $scope.switches.lamps = false;
+                    }
+                    else {
+                        $scope.switches.lamps = true;
+                    }
+                    break;
+              
+                default:
+                    alert('default');
+            }
+            console.log(switchName);
+            console.log($scope.switches);
+        }
+        $scope.defaults = {
+            navbar: [
+                'Technologies',
+                'Education',
+                'Work History',
+                'Certifications',
+                'Contact & Hobbies',
+            ],
+            author: 'Goodson',
+            mission: 'Design and Development'
+
+        };
+
+        // This sets the doors open variable to an empty string. So that the doors are closed when page loads.
+        $scope.doorsOpen = false;
+
+        // Function changes doorsOpen variable to activate the doors opening.
+        $scope.activateDoors = function (content) {
+
+            // If doors open variable is set, unset it.
+            if ($scope.doorsOpen != '') {
+                $scope.doorsOpen = '';
+
+                // Wait 1.9 seconds for doors to close before reopen.
+                $timeout(function () {
+                    $scope.doorsOpen = content;
+                }, 1900);
+            }
+            else {
+
+                // Else open doors.
+                $scope.doorsOpen = content;
+            }
+
+
+
+        };
+    });
---
+++ examples/functions.js
@@ -0,0 +1,18 @@
+﻿
+
+
+//var stands for variable
+//sampleFuntion is the name of the function and is how the function is called into action later 
+//The word function is telling the code that we are making a specific type of var that behaves like a funtion
+//the parenthesis are were we put our parameters which is what we pass into the function for it to use
+//The curly/mustache brackets denote the body of the function which is what runs as the funtion is called and uses the set parameters that are passed in
+var sampleFunction = function () {
+    console.log('this is a function declaration');
+}
+
+
+
+var sampleFunctionWithParameters = function (sampleparameter1, sampleparameter2) {
+    console.log(sampleparameter1);
+    console.log(sampleparameter2);
+}
